2d354472b4005967a79b1d29b23529a533ce6db9
Brilliant2meNu <129690173+Brilliant2meNu@users.noreply.github.com>
2025-01-25 20:08:19 -0800

    Update logout.js

--- auth/logout.js
+++ auth/logout.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Logged out successfully" });
+  return res.status(200).json({ message: "Logged out successfully" });
 }
