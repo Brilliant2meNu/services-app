f9135a7210effb042adc3af7d492c736ae303516
Brilliant2meNu <129690173+Brilliant2meNu@users.noreply.github.com>
2025-02-03 16:43:43 -0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,29 +1,23 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://ovmdpupofhhudxhitoov.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92bWRwdXBvZmhodWR4aGl0b292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwMjQ1MjUsImV4cCI6MjA1MzYwMDUyNX0.r2SgtfiL8c4rTGcXOBPhU9csCztLLjP8ibEXat46gaM"
-);
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { email, password } = req.body;
+  const { firstName, lastName, password } = req.body;
 
   try {
     const { data, error } = await supabase
       .from("users")
       .select("*")
-      .eq("email", email)
+      .eq("first_name", firstName)
+      .eq("last_name", lastName)
       .eq("password", password);
 
     if (error || data.length === 0) {
-      return res.status(401).json({ error: "Invalid email or password" });
+      return res.status(401).json({ error: "Invalid credentials." });
     }
 
-    res.status(200).json({ role: data[0].role });
+    res.status(200).json({ message: "Login successful!", role: data[0].role });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
