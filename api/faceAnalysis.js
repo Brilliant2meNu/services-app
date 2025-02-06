6d891cc1ee04b74919c45109308b5bb2c3364201
Brilliant2meNu <129690173+Brilliant2meNu@users.noreply.github.com>
2025-02-05 19:27:41 -0800

    Update faceAnalysis.js

--- api/faceAnalysis.js
+++ api/faceAnalysis.js
@@ -1,11 +1,11 @@
-import * as faceapi from "../js/face-api.min.js";
+import * as faceapi from "./face-api.min.js"; // Adjusted path for the correct location
 
 export const runFaceAnalysis = async (canvas) => {
   await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
   await faceapi.nets.faceExpressionNet.loadFromUri("/models");
 
   const detections = await faceapi.detectAllFaces(canvas).withFaceExpressions();
-  return detections.map(d => ({
+  return detections.map((d) => ({
     expressions: d.expressions,
     position: d.alignedRect.box,
   }));
