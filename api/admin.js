4e30c788e05cfb6a55eab7778a0c5c969c6d52e9
Brilliant2meNu <129690173+Brilliant2meNu@users.noreply.github.com>
2025-02-05 16:45:50 -0800

    Delete api/admin.js

--- api/admin.js
+++
@@ -1,9 +0,0 @@
-
-// admin.js
-import { initModal } from "./modalHandler.js";
-import { setupEventHandlers } from "./eventHandlers.js";
-import { setupButtonActions } from "./buttonActions.js";
-
-initModal("registerModal", ".tab-button", ".tab-content");
-setupEventHandlers();
-setupButtonActions();
