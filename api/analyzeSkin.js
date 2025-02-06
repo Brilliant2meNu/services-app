2b6e32180fbd15f55ce258d186df5ca9d2b5a4de
Brilliant2meNu <129690173+Brilliant2meNu@users.noreply.github.com>
2025-02-05 19:27:21 -0800

    Update analyzeSkin.js

--- api/analyzeSkin.js
+++ api/analyzeSkin.js
@@ -1,8 +1,13 @@
-import * as faceapi from "../js/face-api.min.js";
+import * as faceapi from "./face-api.min.js"; // Adjusted path for the correct location
 
 export const loadModels = async () => {
   const modelPath = "/models/"; // Path to your models folder
   await faceapi.nets.ssdMobilenetv1.loadFromUri(modelPath);
   await faceapi.nets.faceLandmark68Net.loadFromUri(modelPath);
   await faceapi.nets.faceExpressionNet.loadFromUri(modelPath);
+};
+
+export const analyzeSkin = async () => {
+  console.log("Starting skin analysis...");
+  // Add any logic here for handling the analysis
 };
